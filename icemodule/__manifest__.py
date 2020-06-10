55ae6921ec32ad7a459022a342b9da020af04348
Webimtech <66035639+Webimtech@users.noreply.github.com>
2020-06-10 11:16:18 +0200

    category icemodule version 1.0

--- icemodule/__manifest__.py
+++ icemodule/__manifest__.py
@@ -16,8 +16,8 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': 'icemodule.1.0',
+    'category': 'icemodule',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
